d1b20a1cfeb03475ff442aad586f0ca9cd5fc856
Alex <81312945+VivaciousViolin@users.noreply.github.com>
2021-04-23 09:43:03 -0400

    orifjaoijdfpoiasdjfw

--- translations.py
+++ translations.py
@@ -1,20 +1,13 @@
-from PIL import Image #for image processing
+import PIL.Image #for image processing
 import pytesseract # for photo to text
 import os # for fliepaths and os things
+import docx
 from docx import Document #to write to .docs files   INSTALL python-docx
 import unicodedata #remove unicode to transfer to .docx
 import re # REGEX LET'S GO
-import tkinter as tk
 
 #https://github.com/UB-Mannheim/tesseract/wiki
 
-
-#add color class so you can print in colors easily
-class col:
-    white = '\033[0;37;40m'
-    green = '\033[92m'
-    red = '\033[93m'
-    pink = '\033[95m'
 
 #add a user input to continue
 def inputToContinue(question):
@@ -24,28 +17,119 @@ def inputToContinue(question):
 #create a relative file path to reference
 userprofile = os.environ['USERPROFILE']
 AppDataPath = os.path.join(userprofile, 'AppData')
-print(col.green + "userprofile is: " + userprofile)
+print("userprofile is: " + userprofile)
 
 #set the file location of tesseract
 pytesseract.pytesseract.tesseract_cmd = AppDataPath + "\\local\\programs\\Tesseract-OCR\\tesseract.exe"
 
-
 #write to .docx prerequesites
 document = Document()
 
+##############################################################################################
+##############################################################################################
 
-##############################################################################################
-##############################################################################################
 
 def processImage():
+    #ask folder path
+    imgPath = askopenfilename(title='select your translations document') # shows dialog box and return the path
+    print(imgPath)
+    dirLabel.configure(text="the translation image folderpath is: " + imgPath)
+
     #process the image
-    img = Image.open(os.path.dirname(__file__) + "\\transl.PNG")
-    global text
-    text = pytesseract.image_to_string(img, lang = 'eng')
-    return text
+    text = pytesseract.image_to_string(PIL.Image.open(imgPath), lang = 'eng')
+    print(text)
+    
+    with open('transl.docx', mode ='w'):
+        #re.match("[^a-zA-Z0-9 ,.'\"\n:();]")
+        formattedText = re.sub("[^ -~\"]", " ", text)
+        print(formattedText)
+        document.add_paragraph(formattedText)
+        document.save('transl.docx')
+
+
+def doRevisions():
+    #code asks you to edit before you continue
+    #inputToContinue(col.white + "please press enter to make revisions to the text file")
+
+    #run the text file to edit
+    os.startfile(os.path.dirname(__file__) + "\\transl.docx")
+    os.startfile(os.path.dirname(__file__) + "\\transl.png")
+
+    #asks you to enter once you are done editing
+    #inputToContinue(col.white + "please press enter once you are done with revisions to the document")
+
+
+def translate():
+    # Opening MS word document 
+    doc = docx.Document('transl.docx')
+
+    #Filtering iltalicized text 
+    Italic_Text = '' 
+    for p in doc.paragraphs: 
+        for run in p.runs: 
+            if run.italic: 
+                Italic_Text = Italic_Text + run.text + " " 
+    
+    #Splitting text to a list of words 
+    Italic_Words =list(filter(None,Italic_Text.split(' '))) 
+    print(Italic_Words)
+    #Counting the number of italicized words 
+    counter = len(Italic_Words)
+    print(counter)
+
+##################################################################################################
+##################################################################################################
+import tkinter
+from tkinter import Tk, Label, Button
+from tkinter.filedialog import askopenfilename
+
+#set so you can call tk.whatever
+tk = Tk()
+
+#headers for the thing
+tk.title("Welcome to translation station")
+tk.geometry('600x600')
+
+#what to do when directory button is clicked
+def dirButton():
+    global folderpath
+    folderpath = askopenfilename(title='select your translations image to convert it to a word doc')
+    print(folderpath)
+    dirLabel.configure(text="the translation image folderpath is: " + folderpath)
+    dirBtn.grid(column=0, row=0)
+    return folderpath
+
+
+#the label where the directory will go
+dirLabel = Label(tk)   #, text="Hello"
+dirLabel.grid(column=0, row=1)
+
+#button for photo to text to word doc button
+dirBtn = Button(tk, text="Select your translations image file path to convert it to text", command=processImage)
+dirBtn.grid(column=0, row=0)
+
+#button for opening the word docs
+docBtn = Button(tk, text="Open the word document to highlight your stem words and do revisions", command=doRevisions)
+docBtn.grid(column=0, row=4)
+
+#button to translate
+translBtn = Button(tk, text="convert the highlighted words to stems", command=translate)
+translBtn.grid(column=0, row=6)
+
+
+#run the sucker
+tk.mainloop()
 
 
 
+
+
+
+
+
+
+
+"""
 def writeToDoc(writeBackup):
     #write the text to 'transl.docx' in this directory
     try:
@@ -69,43 +153,4 @@ def writeToDoc(writeBackup):
 
             #write to the console
             print(col.green + "text file written to " + os.path.dirname(__file__) + "\\transl.docx" )
-
-def askForRevisions():
-    #code asks you to edit before you continue
-    inputToContinue(col.white + "please press enter to make revisions to the text file")
-
-    #run the text file to edit
-    os.startfile(os.path.dirname(__file__) + "\\transl.docx")
-    os.startfile(os.path.dirname(__file__) + "\\transl.png")
-
-    #asks you to enter once you are done editing
-    inputToContinue(col.white + "please press enter once you are done with revisions to the document")
-
-##################################################################################################
-##################################################################################################
-def main():
-    processImage()
-    writeToDoc(True)
-    askForRevisions()
-
-main()
-##################################################################################################
-##################################################################################################
-
-border_effects = {
-    "flat": tk.FLAT,
-    "sunken": tk.SUNKEN,
-    "raised": tk.RAISED,
-    "groove": tk.GROOVE,
-    "ridge": tk.RIDGE,
-}
-
-window = tk.Tk()
-
-for relief_name, relief in border_effects.items():
-    frame = tk.Frame(master=window, relief=relief, borderwidth=5)
-    frame.pack(side=tk.LEFT)
-    label = tk.Label(master=frame, text=relief_name)
-    label.pack()
-
-window.mainloop()
+"""
